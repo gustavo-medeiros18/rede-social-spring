9c7fa52f20c7ef742c862cdce0f83993e323539b
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-08-13 13:30:53 -0300

    Initial commit.

---
+++ src/test/java/authproject/AuthProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package authproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthProjectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
