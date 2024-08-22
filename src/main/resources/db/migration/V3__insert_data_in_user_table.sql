e52c29576ee8cbaa3a3f6aa7aa1fa3562028e6ff
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-08-22 13:54:33 -0300

    🗃️ feat: Adds new migration for inserting data in users table.

---
+++ src/main/resources/db/migration/V3__insert_data_in_user_table.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (username, password, email, enabled) VALUES
+('user1', 'password1', 'user1@example.com', TRUE),
+('user2', 'password2', 'user2@example.com', TRUE),
+('user3', 'password3', 'user3@example.com', TRUE),
+('user4', 'password4', 'user4@example.com', TRUE),
+('user5', 'password5', 'user5@example.com', TRUE);
