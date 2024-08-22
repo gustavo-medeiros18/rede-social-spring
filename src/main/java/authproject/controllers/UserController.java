c60537f1c813ad359023712aaec0dbf8746463d4
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-08-22 14:40:33 -0300

    ✨ feat: Replaces "hello world" route with basic GET /users route.

--- src/main/java/authproject/controllers/UserController.java
+++ src/main/java/authproject/controllers/UserController.java
@@ -1,14 +1,22 @@
 package authproject.controllers;
 
+import authproject.models.User;
+import authproject.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
-  @GetMapping("/hello")
-  public String hello() {
-    return "Hello, user!";
+  @Autowired
+  private UserService userService;
+
+  @GetMapping()
+  public List<User> findAll() {
+    return userService.findAll();
   }
 }
