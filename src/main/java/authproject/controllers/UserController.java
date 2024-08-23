f95599ad6726812a710c24644e5f5debb51cb67f
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-08-23 09:37:01 -0300

    ✨ feat: Adds findById() method in UserController.

--- src/main/java/authproject/controllers/UserController.java
+++ src/main/java/authproject/controllers/UserController.java
@@ -4,9 +4,7 @@ import authproject.models.User;
 import authproject.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +17,10 @@ public class UserController {
   @GetMapping()
   public ResponseEntity<List<User>> findAll() {
     return ResponseEntity.ok(userService.findAll());
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<User> findById(@PathVariable("id") Long id) {
+    return ResponseEntity.ok(userService.findById(id));
   }
 }
