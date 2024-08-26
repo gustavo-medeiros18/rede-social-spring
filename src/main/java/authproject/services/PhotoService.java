132556437348ab679833e3e90aff2198dc73e545
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-08-26 14:56:34 -0300

    ✨ feat: Adds PhotoService class with create() method.

---
+++ src/main/java/authproject/services/PhotoService.java
@@ -0,0 +1,37 @@
+package authproject.services;
+
+import authproject.dtos.PhotoDto;
+import authproject.models.Photo;
+import authproject.models.User;
+import authproject.repositories.PhotoRepository;
+import authproject.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
+
+@Service
+public class PhotoService {
+  Logger logger = Logger.getLogger(PhotoService.class.getName());
+  private PhotoRepository photoRepository;
+  private UserRepository userRepository;
+
+  @Autowired
+  public PhotoService(PhotoRepository photoRepository, UserRepository userRepository) {
+    this.photoRepository = photoRepository;
+    this.userRepository = userRepository;
+  }
+
+  public Photo create(PhotoDto photoDto) {
+    logger.info("Creating photo for user with id: " + photoDto.getUserId());
+
+    Photo photo = new Photo();
+    photo.setUrl(photoDto.getUrl());
+    photo.setDescription(photoDto.getDescription());
+
+    User user = userRepository.findById(photoDto.getUserId()).get();
+    photo.setUser(user);
+
+    return photoRepository.save(photo);
+  }
+}
