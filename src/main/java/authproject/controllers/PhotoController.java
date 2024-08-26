43d434ef1680d642ce665f3d5c6c4ad6f99cdd55
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-08-26 15:06:52 -0300

    ✨ feat: Adds findAll() method in PhotoController.

--- src/main/java/authproject/controllers/PhotoController.java
+++ src/main/java/authproject/controllers/PhotoController.java
@@ -5,10 +5,9 @@ import authproject.models.Photo;
 import authproject.services.PhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/photo")
@@ -23,5 +22,10 @@ public class PhotoController {
   @PostMapping()
   public ResponseEntity<Photo> create(@RequestBody PhotoDto photoDto) {
     return ResponseEntity.ok(service.create(photoDto));
+  }
+
+  @GetMapping()
+  public ResponseEntity<List<Photo>> findAll() {
+    return ResponseEntity.ok(service.findAll());
   }
 }
