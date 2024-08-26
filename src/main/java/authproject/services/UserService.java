acdc7909597c5e1457a8d25dba972740d45d9848
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-08-26 13:39:09 -0300

    ✨ feat: Adds new UserService class with findAll() method.

---
+++ src/main/java/authproject/services/UserService.java
@@ -0,0 +1,24 @@
+package authproject.services;
+
+import authproject.models.User;
+import authproject.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+@Service
+public class UserService {
+  private Logger logger = Logger.getLogger(UserService.class.getName());
+  private UserRepository repository;
+
+  @Autowired
+  public UserService(UserRepository repository) {
+    this.repository = repository;
+  }
+
+  public List<User> findAll() {
+    return repository.findAll();
+  }
+}
