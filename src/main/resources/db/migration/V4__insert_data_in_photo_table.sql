aec31da907f4d7d4f54a8d2bbdcd3f20011de374
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-08-26 09:57:29 -0300

    🗃️ feat: Adds new migration for inserting data in photos table.

---
+++ src/main/resources/db/migration/V4__insert_data_in_photo_table.sql
@@ -0,0 +1,6 @@
+INSERT INTO photos (user_id, url, description) VALUES
+(2, 'http://example.com/photo1.jpg', 'Description for photo 1'),
+(3, 'http://example.com/photo2.jpg', 'Description for photo 2'),
+(4, 'http://example.com/photo3.jpg', 'Description for photo 3'),
+(15, 'http://example.com/photo4.jpg', 'Description for photo 4'),
+(19, 'http://example.com/photo5.jpg', 'Description for photo 5');
