469e95fabca43f18d3c68b541ea837ec2371814f
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-08-29 11:32:36 -0300

    🦺 feat: Adds PhotoValidator class.

---
+++ src/main/java/authproject/validators/PhotoValidator.java
@@ -0,0 +1,17 @@
+package authproject.validators;
+
+public class PhotoValidator {
+  private static String urlRegex = "[(http(s)?):\\/\\/(www\\.)?a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)";
+
+  public static boolean urlIsValid(String url) {
+    return url.matches(urlRegex);
+  }
+
+  public static boolean descriptionIsValid(String url) {
+    return url != null && !url.isEmpty();
+  }
+
+  public static boolean userIdIsValid(Long userId) {
+    return userId != null;
+  }
+}
