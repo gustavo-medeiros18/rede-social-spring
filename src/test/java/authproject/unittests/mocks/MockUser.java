afcb975b1ae3a225f83c0cc0575a5d6ffd58c736
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-08-30 11:51:41 -0300

    🤡 test: Adds MockUser class for mocking user fields.

---
+++ src/test/java/authproject/unittests/mocks/MockUser.java
@@ -0,0 +1,34 @@
+package authproject.unittests.mocks;
+
+import authproject.models.User;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MockUser {
+  public User mockEntity() {
+    return mockEntity(0);
+  }
+
+  public User mockEntity(Integer number) {
+    User user = new User();
+
+    user.setUsername("usernametest" + number);
+    user.setEmail("emailtest" + number + "@test.com");
+    user.setPassword("passwordtest" + number);
+    user.setEnabled(true);
+    user.setCreatedAt(Instant.parse("2024-01-01T00:00:00Z"));
+    user.setUpdatedAt(Instant.parse("2024-01-01T00:00:00Z"));
+
+    return user;
+  }
+
+  public List<User> mockEntityList() {
+    List<User> users = new ArrayList<User>();
+    for (int i = 0; i < 15; i++)
+      users.add(mockEntity(i));
+
+    return users;
+  }
+}
