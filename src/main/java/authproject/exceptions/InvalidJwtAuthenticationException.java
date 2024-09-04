0aec415be3537ca7ba3e698e6b15487f45f685c3
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-09-04 11:52:56 -0300

    🥅 feat: Adds InvalidJwtAuthenticationException class.

---
+++ src/main/java/authproject/exceptions/InvalidJwtAuthenticationException.java
@@ -0,0 +1,13 @@
+package authproject.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.naming.AuthenticationException;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class InvalidJwtAuthenticationException extends AuthenticationException {
+  public InvalidJwtAuthenticationException(String message) {
+    super(message);
+  }
+}
