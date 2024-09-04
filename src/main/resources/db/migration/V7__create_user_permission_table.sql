4918a2659a0e873ff7edac8f379846de21c73aec
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-09-04 15:31:42 -0300

    🗃️ feat: Adds migration to create user_permission pivot table.

---
+++ src/main/resources/db/migration/V7__create_user_permission_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE user_permission (
+    id_user BIGINT NOT NULL,
+    id_permission BIGINT NOT NULL,
+    PRIMARY KEY (id_user, id_permission),
+    KEY fk_user_permission_permission (id_permission),
+    CONSTRAINT fk_user_permission FOREIGN KEY (id_user) REFERENCES users (id),
+    CONSTRAINT fk_user_permission_permission FOREIGN KEY (id_permission) REFERENCES permissions (id)
+);
