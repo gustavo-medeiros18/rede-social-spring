057809df1350b7e837a644394e9ca5aff94c433f
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-09-04 15:29:56 -0300

    🛂 feat: Makes User model class implement the UserDetails interface.

--- src/main/java/authproject/models/User.java
+++ src/main/java/authproject/models/User.java
@@ -2,14 +2,16 @@ package authproject.models;
 
 import com.fasterxml.jackson.annotation.*;
 import jakarta.persistence.*;
-import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.SourceType;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.hateoas.RepresentationModel;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 
@@ -17,7 +19,7 @@ import java.util.Objects;
 @Table(name = "users")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
-public class User extends RepresentationModel<User> implements Serializable {
+public class User extends RepresentationModel<User> implements UserDetails, Serializable {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "id", nullable = false)
@@ -31,6 +33,15 @@ public class User extends RepresentationModel<User> implements Serializable {
 
   @Column(name = "email", nullable = false, length = 100)
   private String email;
+
+  @Column(name = "account_non_expired")
+  private Boolean accountNonExpired;
+
+  @Column(name = "account_non_locked")
+  private Boolean accountNonLocked;
+
+  @Column(name = "credentials_non_expired")
+  private Boolean credentialsNonExpired;
 
   @Column(name = "enabled", nullable = false)
   private Boolean enabled = true;
@@ -46,8 +57,21 @@ public class User extends RepresentationModel<User> implements Serializable {
   @Column(name = "updated_at", nullable = false)
   private Instant updatedAt;
 
+  @ManyToMany(fetch = FetchType.EAGER)
+  @JoinTable(
+      name = "user_permission",
+      joinColumns = @JoinColumn(name = "id_user"),
+      inverseJoinColumns = @JoinColumn(name = "id_permission")
+  )
+  private List<Permission> permissions;
+
   public User() {
 
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return this.permissions;
   }
 
   public Long getId() {
@@ -84,6 +108,30 @@ public class User extends RepresentationModel<User> implements Serializable {
 
   public Boolean getEnabled() {
     return enabled;
+  }
+
+  public Boolean getAccountNonExpired() {
+    return accountNonExpired;
+  }
+
+  public void setAccountNonExpired(Boolean accountNonExpired) {
+    this.accountNonExpired = accountNonExpired;
+  }
+
+  public Boolean getAccountNonLocked() {
+    return accountNonLocked;
+  }
+
+  public void setAccountNonLocked(Boolean accountNonLocked) {
+    this.accountNonLocked = accountNonLocked;
+  }
+
+  public Boolean getCredentialsNonExpired() {
+    return credentialsNonExpired;
+  }
+
+  public void setCredentialsNonExpired(Boolean credentialsNonExpired) {
+    this.credentialsNonExpired = credentialsNonExpired;
   }
 
   public void setEnabled(Boolean enabled) {
