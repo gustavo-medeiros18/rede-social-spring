d71e2b2afb7e33caeeb207b8cb473418b9f7e448
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-09-05 11:42:49 -0300

    🛂 feat: Adds custom query in UserRepository to find a user by its username.

--- src/main/java/authproject/repositories/UserRepository.java
+++ src/main/java/authproject/repositories/UserRepository.java
@@ -2,9 +2,12 @@ package authproject.repositories;
 
 import authproject.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-  User findByUsername(String username);
+  @Query("SELECT u FROM User u WHERE u.username = :username")
+  User findByUsername(@Param("username") String username);
 }
