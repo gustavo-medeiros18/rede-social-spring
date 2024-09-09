01a9fc68852e89ca14223921ac94c6f76658972a
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-09-09 13:44:49 -0300

    :sparkles: feat: Adds UserDto class.

---
+++ src/main/java/authproject/dtos/UserDto.java
@@ -0,0 +1,62 @@
+package authproject.dtos;
+
+import java.util.Objects;
+
+public class UserDto {
+  private String username;
+  private String password;
+  private String email;
+
+  public UserDto() {
+
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    UserDto userDto = (UserDto) o;
+    return Objects.equals(username, userDto.username) &&
+        Objects.equals(password, userDto.password) &&
+        Objects.equals(email, userDto.email);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(username, password, email);
+  }
+
+  @Override
+  public String toString() {
+    return "UserDto{" +
+        "username='" + username + '\'' +
+        ", password='" + password + '\'' +
+        ", email='" + email + '\'' +
+        '}';
+  }
+}
