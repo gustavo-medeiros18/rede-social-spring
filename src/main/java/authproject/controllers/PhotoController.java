7b1979f8547a0355e758eb88ca76c2ce8d0c2206
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-09-11 14:02:40 -0300

    ✨ feat: Updates PhotoController.update() method to receive multipart data instead of RequestBody.

--- src/main/java/authproject/controllers/PhotoController.java
+++ src/main/java/authproject/controllers/PhotoController.java
@@ -44,8 +44,18 @@ public class PhotoController {
     return ResponseEntity.ok(service.findSingle(id));
   }
 
-  @PutMapping("/{id}")
-  public ResponseEntity<Photo> update(@PathVariable Long id, @RequestBody PhotoDto photoDto) {
+  @PutMapping(path = "/{id}", consumes = {"multipart/form-data"})
+  public ResponseEntity<Photo> update(
+      @PathVariable Long id,
+      @RequestParam("description") String description,
+      @RequestParam("userId") Long userId,
+      @RequestParam("imageFile") MultipartFile imageFile
+  ) {
+    PhotoDto photoDto = new PhotoDto();
+    photoDto.setDescription(description);
+    photoDto.setUserId(userId);
+    photoDto.setImageFile(imageFile);
+
     Photo updatedPhoto = service.update(id, photoDto);
 
     if (updatedPhoto == null) return ResponseEntity.notFound().build();
