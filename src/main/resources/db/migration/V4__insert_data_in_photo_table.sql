4b809483af1ef829b448d16e7cbeaf01c70d1bb3
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2025-01-08 18:26:03 -0300

    🐛 bug: Corrects user_id foreign keys in photo table seeding.

--- src/main/resources/db/migration/V4__insert_data_in_photo_table.sql
+++ src/main/resources/db/migration/V4__insert_data_in_photo_table.sql
@@ -1,6 +1,6 @@
 INSERT INTO photos (user_id, url, description) VALUES
-(2, 'http://example.com/photo1.jpg', 'Description for photo 1'),
-(3, 'http://example.com/photo2.jpg', 'Description for photo 2'),
-(4, 'http://example.com/photo3.jpg', 'Description for photo 3'),
-(15, 'http://example.com/photo4.jpg', 'Description for photo 4'),
-(19, 'http://example.com/photo5.jpg', 'Description for photo 5');
+(1, 'http://example.com/photo1.jpg', 'Description for photo 1'),
+(2, 'http://example.com/photo2.jpg', 'Description for photo 2'),
+(3, 'http://example.com/photo3.jpg', 'Description for photo 3'),
+(4, 'http://example.com/photo4.jpg', 'Description for photo 4'),
+(5, 'http://example.com/photo5.jpg', 'Description for photo 5');
